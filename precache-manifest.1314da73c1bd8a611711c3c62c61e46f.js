5e7433ddea8a98dd5beea177cd6e9022adbb4f58
unknown <vaishnavikhumbhar@example.com>
2025-08-20 14:40:05 +0530

    Updates

---
+++ precache-manifest.1314da73c1bd8a611711c3c62c61e46f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "095deb70567f67114a93be7126c90144",
+    "url": "/vaishnavi-khumbhar/ProText.git/index.html"
+  },
+  {
+    "revision": "cf75323a38f46092119a",
+    "url": "/vaishnavi-khumbhar/ProText.git/static/css/main.6969e0f2.chunk.css"
+  },
+  {
+    "revision": "4273953c4efd8b5ec621",
+    "url": "/vaishnavi-khumbhar/ProText.git/static/js/2.0f04fd15.chunk.js"
+  },
+  {
+    "revision": "99562942502e41af72fc73d84afe4332",
+    "url": "/vaishnavi-khumbhar/ProText.git/static/js/2.0f04fd15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c171ad7b0c2b85c60dd",
+    "url": "/vaishnavi-khumbhar/ProText.git/static/js/3.495dc4e5.chunk.js"
+  },
+  {
+    "revision": "cf75323a38f46092119a",
+    "url": "/vaishnavi-khumbhar/ProText.git/static/js/main.10742371.chunk.js"
+  },
+  {
+    "revision": "44e31e4adb11d23c10d3",
+    "url": "/vaishnavi-khumbhar/ProText.git/static/js/runtime-main.8e9c545f.js"
+  }
+]);
